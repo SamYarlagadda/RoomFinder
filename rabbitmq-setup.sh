9fcd8f7bf187c9f560a3ff74abb21059c76c314b
Bowmen0103 <98347261+Bowmen0103@users.noreply.github.com>
2024-02-26 13:39:19 -0500

    Create rabbitmq-setup.sh
    Created what should be the composer script to install and initialize rabbitmq and enable communication over port 7007

---
+++ rabbitmq-setup.sh
@@ -0,0 +1,10 @@
+#COMPOSER SCRIPT
+#ONLY TO BE RUN ON UBUNTU-SERVER TO INSTALL AND INITIALIZE RABBITMQ
+sudo apt update
+sudo apt install -y rabbitmq-server
+
+echo "listeners.tcp.default = 7007" | sudo tee -a /etc/rabbitmq.conf
+
+sudo systemctl restart rabbitmq-server
+
+sudo rabbitmqctl status
