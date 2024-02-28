b3e75b832c1596372731a4bdc8d345245fc745d4
Bowmen0103 <98347261+Bowmen0103@users.noreply.github.com>
2024-02-27 20:22:07 -0500

    Update rabbitmq-setup.sh

--- rabbitmq-setup.sh
+++ rabbitmq-setup.sh
@@ -8,3 +8,5 @@ echo "listeners.tcp.default = 7007" | sudo tee -a /etc/rabbitmq.conf
 sudo systemctl restart rabbitmq-server
 
 sudo rabbitmqctl status
+
+#PRIOR TO RUNNING, RUN THE FOLLOWING IN THE TERMINAL "chmod +x rabbitmq-setup.sh"
