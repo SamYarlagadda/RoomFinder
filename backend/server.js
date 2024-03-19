359a579aa4fc277f6919c2997d98ed5ad6a8eb6f
SamYarlagadda <98673593+SamYarlagadda@users.noreply.github.com>
2024-03-18 22:50:15 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const mysql = require('mysql');
 const app = express()
 const cors = require('cors');
 app.use(cors());
@@ -9,7 +9,22 @@ const port = 3000; // Set your desired port number
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+const pool = mysql.createPool({
+  host: '192.168.7.169',
+  user: 'samuel',
+  password: 'ssy22',
+  database: 'RoomFinderDB',
+  port: '3306',
+  connectTimeout: '20000',
+})
 
+app.get('/users', (req,res)=>{
+  const sql = 'select * from users';
+  pool.query(sql,(err,data)=>{
+    if(err) return res.json(err);
+    return res.json(data);
+  })
+})
 
 app.get('/adduser', (req, res) => {
   console.log(req.body);
