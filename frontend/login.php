724f421a9700e1645f16faccd929402abb75954b
Kaan-KB <95941377+Kaan-KB@users.noreply.github.com>
2024-04-23 22:45:12 +0000

    commit

--- frontend/login.php
+++ frontend/login.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="UTF-8" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="mystyle.css">
-    <script src="login.js"></script>
   </head>
   <body>
         <?php
@@ -13,8 +12,29 @@
 
             $connection = new AMQPStreamConnection('10.241.141.94', '5672', 'ssy22', 'ssy22', 'ssy22');
             $channel = $connection->channel();
-            $channel->queue_declare('frontend', false, false, false, false);
+            $channel->queue_declare('login_request', false, false, false, false);
+            $callback_queue = uniqid('login_response');
+            $channel->queue_declare($callback_queue, false, false, false, false);
             
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+              $username = $_POST['username'];
+              $id = $_POST['njit_id'];
+              $password = md5($password);
+
+              $data = [
+                  'username' => $username,
+                  'password' => $password,
+                  'njit_id' => $id
+              ];
+
+              $msg = new AMQPMessage(json_encode($data), ['reply_to' => $callback_queue]);
+
+
+              $channel->basic_publish($msg, '', 'login_request');
+
+
+
             $channel->close();
             $connection->close();
             ?>
