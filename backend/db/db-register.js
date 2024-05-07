2246125c9b927e7ae1708dd10aa0fd5e2a039b79
Sam Yarlagadda <ssy22@njit.edu>
2024-05-07 19:43:11 +0000

    package.json

--- backend/db/db-register.js
+++ backend/db/db-register.js
@@ -1,7 +1,6 @@
 const amqp = require('amqplib/callback_api');
 const mysql = require('mysql');
 
-// Create a new pool instance
 const pool = mysql.createPool({
     host: '10.241.130.162',
     user: 'rp855',
@@ -20,8 +19,13 @@ amqp.connect('amqp://ssy22:ssy22@10.241.141.94/ssy22', function(error0, connecti
         }
 
         let dbQueue = 'db_register';
+        let responseQueue = 'register_response';
 
         channel.assertQueue(dbQueue, {
+            durable: false
+        });
+
+        channel.assertQueue(responseQueue, {
             durable: false
         });
 
@@ -30,24 +34,24 @@ amqp.connect('amqp://ssy22:ssy22@10.241.141.94/ssy22', function(error0, connecti
         channel.consume(dbQueue, function(msg) {
             console.log(" [x] Received %s", msg.content.toString());
 
-            // Parse the message content to JSON
             let credentials = JSON.parse(msg.content.toString());
 
-            // Query the database
             pool.query('SELECT * FROM users WHERE username = ? AND njit_id = ?', [credentials.username, credentials.njit_id], (err, results) => {
                 if(err) {
                     console.log(err);
                 } else {
                     if(results.length > 0) {
                         console.log('User exists');
+                        // Optionally, you can send a 'failed' message if the user already exists
+                        channel.sendToQueue(responseQueue, Buffer.from('unsuccessful'));
                     } else {
                         console.log('User does not exist');
-                        // Insert the new user into the database
                         pool.query('INSERT INTO users SET ?', credentials, (err, res) => {
                             if(err) {
                                 console.log('Error inserting new user:', err);
                             } else {
                                 console.log('Inserted new user with id:', res.insertId);
+                                channel.sendToQueue(responseQueue, Buffer.from('successful'));
                             }
                         });
                     }
